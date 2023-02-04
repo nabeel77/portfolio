98b56d5b7a255bda05c0cda982b51a8a429cf016
Nabeel Munir <nabeel.77.dev@Nabeels-MacBook-Air.local>
2023-02-05 01:31:06 +0200

    fix: fixed logger issue

--- server/logger/index.js
+++ server/logger/index.js
@@ -1,17 +1,22 @@
 const { format, createLogger, transports } = require('winston');
 const { timestamp, combine, errors, json } = format;
 
-const logger = createLogger({
-  format: combine(timestamp(), errors({ stack: true }), json()),
-  defaultMeta: { service: 'portfolio' },
-  transports: [
-    /*
-        - Write all logs with level `error` and below to `error.log`
-        - Write all logs with level `info` and below to `combined.log`
-        */
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/combined.log' }),
-  ],
-});
+const createLoggerInstance = () => {
+  try {
+    const logger = createLogger({
+      format: combine(timestamp(), errors({ stack: true }), json()),
+      defaultMeta: { service: 'portfolio' },
+      transports: [
+        /*
+            - Write all logs with level `error` and below to `error.log`
+            - Write all logs with level `info` and below to `combined.log`
+            */
+        new transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new transports.File({ filename: 'logs/combined.log' }),
+      ],
+    });
+    return logger;
+  } catch (err) {}
+};
 
-exports.logger = logger;
+export default createLoggerInstance;
