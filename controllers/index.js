0672e9e0132abe96bd6699fc30ba99b782fcdc71
Nabeel Munir <nabeel.77.dev@Nabeels-MacBook-Air.local>
2023-02-05 16:29:49 +0200

    fix: fixed custom server

--- controllers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as MessageController } from './emailController';
-export { checkUser, autheticateUser } from './userController';
-export { skills } from './skillsController';
-export { getUserProjects, addUserProject } from './projectsController';
