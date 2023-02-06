3196b2a0f1b52842be313b2bc4bcabeef60630e8
Nabeel Munir <nabeel.77.dev@Nabeels-MacBook-Air.local>
2023-02-06 13:34:48 +0200

    fix: fixed contact email template images

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -3,6 +3,7 @@ import Input from '../../components/Input';
 import { useRouter } from 'next/router';
 import useForm from '../../components/hooks/useForm';
 import globalDesigns from '../../staticData/globalDesigns';
+import Head from 'next/head';
 
 const AdminLogin = () => {
   const router = useRouter();
@@ -30,38 +31,43 @@ const AdminLogin = () => {
     }
   };
   return (
-    <div className="flex flex-col justify-center items-center gap-10 w-full h-screen">
-      <h2 className="text-base lg:text-2xl font-OrbitronBold">Admin Login</h2>
-      <div className="login-form-container bg-base-200 z-50 my-0 ml-[395px] p-5 flex m-4 rounded shadow shadow-3xl -translate-y-4/4 -translate-x-2/4 hover:shadow-5xl h-50vh w-full max-w-sm lg:w-1/3">
-        <form
-          onSubmit={submitHandler}
-          className="p-5 w-full flex flex-col justify-center items-center gap-4"
-        >
-          <Input
-            id="username"
-            element="input"
-            type="text"
-            placeholder="Username"
-            onInput={inputHandler}
-            classNames={inputStyles}
-          />
-          <Input
-            id="password"
-            element="input"
-            type="password"
-            placeholder="Password"
-            onInput={inputHandler}
-            classNames={inputStyles}
-          />
-          <button
-            type="submit"
-            className={`${globalDesigns.responsiveFontStyles} text-primary bg-accent w-1/2 rounded py-2.5 uppercase border border-solid border-blue-darker hover:bg-transparent hover:text-blue-darker`}
+    <React.Fragment>
+      <Head>
+        <title>Admin Login</title>
+      </Head>
+      <div className="flex flex-col justify-center items-center gap-10 w-full h-screen">
+        <h2 className="text-base lg:text-2xl font-OrbitronBold">Admin Login</h2>
+        <div className="login-form-container bg-base-200 z-50 my-0 ml-[395px] p-5 flex m-4 rounded shadow shadow-3xl -translate-y-4/4 -translate-x-2/4 hover:shadow-5xl h-50vh w-full max-w-sm lg:w-1/3">
+          <form
+            onSubmit={submitHandler}
+            className="p-5 w-full flex flex-col justify-center items-center gap-4"
           >
-            Login
-          </button>
-        </form>
+            <Input
+              id="username"
+              element="input"
+              type="text"
+              placeholder="Username"
+              onInput={inputHandler}
+              classNames={inputStyles}
+            />
+            <Input
+              id="password"
+              element="input"
+              type="password"
+              placeholder="Password"
+              onInput={inputHandler}
+              classNames={inputStyles}
+            />
+            <button
+              type="submit"
+              className={`${globalDesigns.responsiveFontStyles} text-primary bg-accent w-1/2 rounded py-2.5 uppercase border border-solid border-blue-darker hover:bg-transparent hover:text-blue-darker`}
+            >
+              Login
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
